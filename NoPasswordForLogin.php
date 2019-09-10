2d1a3b2d25e3eb54196db0d03cf7a5005a3f10ea
Loreto Gabawa Jr <jun.rej@gmail.com>
2019-09-10 22:49:01 +0800

    update

--- NoPasswordForLogin.php
+++ NoPasswordForLogin.php
@@ -9,114 +9,51 @@
    License: GPL2
    */
 
- class NoPasswordForLogin
- {
-   protected $pluginPath;
-   protected $pluginUrl;
-   protected $option_name;
-   protected $table_name = 'npfl_users';
-   public function __construct() {
+class NoPasswordForLogin
+{
+  protected $pluginPath;
+  protected $pluginUrl;
 
-     $this->pluginPath = dirname(__FILE__);
-     $this->pluginUrl = WP_PLUGIN_URL . '/NoPasswordForLogin';
-     $this->option_name = 'npfl_allowed_pages' ;
+  public function __construct() {
+    $this->pluginPath = dirname(__FILE__);
+    $this->pluginUrl = WP_PLUGIN_URL . '/NoPasswordForLogin';
 
-     register_activation_hook( __FILE__, [ $this, 'plugin_activate' ] );
+    add_shortcode('npfl_signup', [ $this, 'signup_form' ] );
+    add_action( 'wp_loaded', [ $this, 'form_submit' ] );
+  }
+  public function signup_form() {    
+    ob_start();
+    include dirname( __FILE__ ) . DIRECTORY_SEPARATOR . 'NoPasswordForLogin_shortcode.php';
+    return ob_get_clean();
+  }
+  public function form_submit() {
+    if ( !is_admin() ) {
+      $retrieved_nonce = $_REQUEST['_wpnonce'];
+      $user_email = $_POST[ 'email' ];
+      $red_url = $_POST[ 'npfl_redirect' ];
 
-     add_action( 'admin_menu', function() {
-       add_options_page( 'NoPasswordForLogin', 'No Password For Login', 'manage_options', 'no-password-for-login', [ $this, 'admin_options_page' ] );
-     });
-
-     add_shortcode('NoPasswordForLogin', [ $this, 'shortcode' ] );
-     add_action( 'wp_loaded', [ $this, 'form_submit' ] );
-
-     add_action( 'wp_loaded', [ $this, 'check_and_redirect' ] );
-   }
-   public function plugin_activate() {
-    global $wpdb;
-    $npfl_db_version = "1.0";
-
-    $table_name = $wpdb->prefix . $this->table_name;
-
-    $charset_collate = $wpdb->get_charset_collate();
-
-    $sql = "CREATE TABLE {$table_name} (
-    id mediumint(9) NOT NULL AUTO_INCREMENT,
-    time datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
-    email varchar(50) NOT NULL,
-    PRIMARY KEY  (id)
-    ) $charset_collate;";
-
-    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-    dbDelta( $sql );
-
-    add_option( 'npfl_db_version', $npfl_db_version );
-
-   }
-   function admin_options_page() {
-     if ( !current_user_can( 'manage_options' ) )  {
-       wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
-     }
-
-     if( isset( $_POST[ 'npfl_form_update_pages' ] ) ) {
-       $new_value = $_POST[ 'page_id' ];
-       if ( get_option( $this->option_name ) !== false ) {
-           update_option( $this->option_name, $new_value );
-       } else {
-           $deprecated = null;
-           $autoload = 'no';
-           add_option( $this->option_name, $new_value, $deprecated, $autoload );
-       }
-     }
-     $page_id = get_option( $this->option_name );
-     include dirname( __FILE__ ) . '\NoPasswordForLogin_options.php';
-   }
-   public function shortcode( $atts ) {
-     ob_start();
-     include dirname( __FILE__ ) . '\NoPasswordForLogin_shortcode.php';
-     return ob_get_clean();
-   }
-
-   public function form_submit() {
-     if ( !is_admin() ) {
-       $retrieved_nonce = $_REQUEST['_wpnonce'];
-       $email = $_POST[ 'email' ];
-       if( isset( $_POST[ 'NoPasswordForLogin' ] ) && wp_verify_nonce($retrieved_nonce, 'save_subscriber' ) ) {
-          if( is_email( $email ) ) {
-            global $wpdb;
-            if( ! $wpdb->get_var("SELECT COUNT(*) FROM {$this->table_name} WHERE email = '$email' ") ) {
-              $wpdb->insert( $wpdb->prefix . $this->table_name, array(
-                'email' => $email
-              ));
-            }
-            setcookie( "npfl_user", $email, time() + (86400 * 30 * 30), "/");
-            $this->redirect_to_page();
+      if( isset( $_POST[ 'npfl_signup' ] ) && wp_verify_nonce($retrieved_nonce, 'save_subscriber' ) ) {
+        $user_name = $user_email;
+        $password = md5( $user_email );
+        if( is_email( $user_email ) ) {
+          if( email_exists($user_email) ) {
+            // create user                        
+            $user = get_user_by( 'email', $user_email );
+            wp_set_password( $password, $user->id );
           } else {
-            $_SESSION[ 'npfl_message' ] = "Invalid email";
+            $user_id = wp_create_user( $user_name, $password, $user_email );
           }
-       }
-     }
-   }
-   public function redirect_to_page() {
-     //$page_id = get_option( $this->option_name );
-     $redirect_uri = add_query_arg ('npfl_redirect', '1', get_permalink ( get_option( $this->option_name ) ));
-     wp_redirect( $redirect_uri ); exit;
-   }
-   public function check_and_redirect() {
-     if ( !is_admin() ) {
-       //$current_url = home_url(add_query_arg(array($_GET), $wp->request));
-       //$redirect_url = get_permalink( get_option( $this->option_name ) );
-
-
-       //echo $current_url . " - " . $redirect_url; exit;
-       if( isset( $_COOKIE[ 'npfl_user' ] ) && !empty( $_COOKIE[ 'npfl_user' ] ) ) {
-         if( !isset( $_GET[ 'npfl_redirect' ] ) ) {
-           $this->redirect_to_page();
-         }
-       }
-     }
+          // login directly
+          wp_signon( [ 'user_login' => $user_email, 'user_password' => $password, 'remember' => true ] );
+          wp_redirect( $red_url );
+          exit;
+        } else {
+          $_SESSION[ 'npfl_message' ] = "Invalid email.";
+        }        
+      }        
    }
  }
+}
 
- $nopasswordforlogin = new NoPasswordForLogin();
+$nopasswordforlogin = new NoPasswordForLogin();
 ?>
--- NoPasswordForLogin_shortcode.php
+++ NoPasswordForLogin_shortcode.php
@@ -1,7 +1,8 @@
 <?php if( isset( $_SESSION[ 'npfl_message' ] ) && !empty( $_SESSION[ 'npfl_message' ] ) ) echo $_SESSION[ 'npfl_message' ]; ?>
 <form method="post">
   <?php wp_nonce_field('save_subscriber'); ?>
-  <input type="hidden" name="NoPasswordForLogin" value="1">
-  <input type="email" name="email" placeholder="Enter your email">
+  <input type="hidden" name="npfl_signup" value="1">
+  <input type="hidden" name="npfl_redirect" value="<?php echo get_permalink() ?>?confirm=<?php echo md5( time() ) ?>">
+  <input type="email" name="email" placeholder="Enter your email" required>
   <input type="submit" value="Register">
 </form>
